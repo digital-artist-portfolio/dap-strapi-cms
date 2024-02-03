94c3d6dd324e339cd90326b781330dade29a1059
SILVAWesley <wesley.silva@ccc.ufcg.edu.br>
2024-02-03 12:22:03 -0300

    chore(database): add an option to disable ssl when connecting to postgres dbs

--- config/database.js
+++ config/database.js
@@ -60,6 +60,7 @@ module.exports = ({ env }) => {
         database: env("DATABASE_NAME", "strapi"),
         user: env("DATABASE_USERNAME", "strapi"),
         password: env("DATABASE_PASSWORD", "strapi"),
+        ssl: env("DATABASE_SSL", false),
       },
       useNullAsDefault: true,
     },
