585dd1be12ee9760af9576d0531a27d05d13e644
SILVAWesley <wesley.silva@ccc.ufcg.edu.br>
2024-02-15 16:34:31 -0300

    chore(plugins): add credentials object

--- config/plugins.js
+++ config/plugins.js
@@ -4,8 +4,10 @@ module.exports = ({ env }) => ({
       provider: "aws-s3",
       providerOptions: {
         s3Options: {
-          accessKeyId: env("AWS_ACCESS_KEY_ID"),
-          secretAccessKey: env("AWS_ACCESS_SECRET"),
+          credentials: {
+            accessKeyId: env("AWS_ACCESS_KEY_ID"),
+            secretAccessKey: env("AWS_ACCESS_SECRET"),
+          },
           region: env("AWS_REGION"),
           params: {
             Bucket: env("AWS_BUCKET"),
