a1a32dbb50f6e1ded981e44b97b2f2ba41495ab0
SILVAWesley <wesley.silva@ccc.ufcg.edu.br>
2024-02-15 16:58:58 -0300

    chore(plugins): remove credentials object

--- config/plugins.js
+++ config/plugins.js
@@ -3,15 +3,11 @@ module.exports = ({ env }) => ({
     config: {
       provider: "aws-s3",
       providerOptions: {
-        s3Options: {
-          credentials: {
-            accessKeyId: env("AWS_ACCESS_KEY_ID"),
-            secretAccessKey: env("AWS_ACCESS_SECRET"),
-          },
-          region: env("AWS_REGION"),
-          params: {
-            Bucket: env("AWS_BUCKET"),
-          },
+        accessKeyId: env("AWS_ACCESS_KEY_ID"),
+        secretAccessKey: env("AWS_ACCESS_SECRET"),
+        region: env("AWS_REGION"),
+        params: {
+          Bucket: env("AWS_BUCKET"),
         },
       },
     },
